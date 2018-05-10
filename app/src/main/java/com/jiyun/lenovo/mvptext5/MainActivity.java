cd036c29322c0edc37cd00198189427996145d8b
GSSHUO <39123771+GSSHUO@users.noreply.github.com>
2018-05-10 08:09:59 +0800

    2018年5月10日08:09:44 提交人：高硕

---
+++ app/src/main/java/com/jiyun/lenovo/mvptext5/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jiyun.lenovo.mvptext5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //爷爷在此，谁敢放肆！
+    }
+}
